f081bde589face0006da4f9fc4958f5d434e44b5
Victor Castro <victorcastro@alu.ufc.br>
2020-03-05 01:17:25 -0300

    saveend

--- Projeto 3/Moto/src/moto/Moto.java
+++ Projeto 3/Moto/src/moto/Moto.java
@@ -1,36 +1,36 @@
-package carro;
+package moto;
 
 import java.util.Scanner;
 
-public class Carro {
+public class Moto {
 	int nPessoas = 0;
-	int maxPessoas = 5;
+	int maxPessoas = 2;
         float gasolina = 0;
-        float maxGasolina = 50;
+        float maxGasolina = 16;
         float km = 0;
         
-	void embarcar(int qtd) {
+	void subir(int qtd) {
             if(qtd < 0){
                 return;
             }
             if(qtd + this.nPessoas > this.maxPessoas){
-                System.out.println("Lotado, mals!");
+                System.out.println("Não da pra subir!");
                 return;
             }
 		this.nPessoas += qtd;
-                System.out.println("Embarcado(os)!");
+                System.out.println("Montado(os)!");
 	}
         
-        void desembarcar(int qtd){
+        void descer(int qtd){
             if(qtd < 0){
                 return;
             }
             if(this.nPessoas < qtd){
-                System.out.println("Há " + this.nPessoas + " pessoas no carro, não é possível descer isso tudo!");
+                System.out.println("Só existe " + this.nPessoas + "pessoas na moto, não é possível descer isso tudo!");
                 return;
             }       
                 this.nPessoas -= qtd;
-                System.out.println("Descembarcado(os)!");
+                System.out.println("Descido(os)!");
         }
 	
         void abastecer(float qtd){
@@ -43,48 +43,47 @@ public class Carro {
                 System.out.println("Abastecido!");
                 return;
         }
-		
-                
-        void dirigir(float qtd){
+		                
+        void pilotar(float qtd){
             if(this.nPessoas == 0){
-                System.out.println("Não há ninguem para dirigir o carro.");
+                System.out.println("Não há ninguem para pilotar a moto.");
             }else if(this.gasolina < (qtd/10)){
                 System.out.println("Gasolina insuficiente para realizar o trajeto!");
             }else{
-                this.gasolina = this.gasolina - qtd / 10;
+                this.gasolina = this.gasolina - qtd / 40;
                 this.km += qtd;
                 System.out.println("Trajeto realizado com sucesso!");
             }
         }
         
 	void mostrar() {
-		System.out.println("Número de pessoas no carro: " + this.nPessoas);
+		System.out.println("Número de pessoas na moto: " + this.nPessoas);
                 System.out.println("O tanque contém: " + this.gasolina + " litros de gasolina.");
                 System.out.println("Km rodados: " + this.km);
 	}
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Carro carro = new Carro();
+		Moto moto = new Moto();
 		while(true) {
 			String line = scanner.nextLine();
 			String ui[] = line.split(" ");
 			if(ui[0].equals("sair")) {
 				break;
 			}else if(ui[0].equals("mostrar")) {
-                            carro.mostrar();
-			}else if(ui[0].equals("embarcar")) {
+                            moto.mostrar();
+			}else if(ui[0].equals("subir")) {
                             int qtd = Integer.parseInt(ui[1]);
-                            carro.embarcar(qtd);
-			}else if(ui[0].equals("desembarcar")){
+                            moto.subir(qtd);
+			}else if(ui[0].equals("descer")){
                             int qtd = Integer.parseInt(ui[1]);
-                            carro.desembarcar(qtd);
+                            moto.descer(qtd);
                         }else if(ui[0].equals("abastecer")){
                             int qtd = Integer.parseInt(ui[1]);
-                            carro.abastecer(qtd);
-                        }else if(ui[0].equals("dirigir")){
+                            moto.abastecer(qtd);
+                        }else if(ui[0].equals("pilotar")){
                             int qtd = Integer.parseInt(ui[1]);
-                            carro.dirigir(qtd);
+                            moto.pilotar(qtd);
                         }else {
 				System.out.println("Comando invalido");
 			}
